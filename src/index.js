7482b4472b6763142c2b51cfd0e2c8a5a1d4bc24
Rebzzel <rebzzel@gmail.com>
2023-11-12 12:39:30 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,28 +1,38 @@
+const SHARED_TWEAKS = [
+  "tweaks/disable-mini-guide-renderer.css",
+  "tweaks/disable-videos-column.css",
+]
+const HOME_PAGE_TWEAKS = [
+  "tweaks/disable-video-browser.css",
+  "tweaks/disable-shorts-browser.css",
+]
+
 function verifyTab(tab) {
   return tab.url.startsWith("https://www.youtube.com")
 }
 
-chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+function isHomePageURL(url) {
+  return url === "https://www.youtube.com/"
+}
+
+async function handleTabUpdate(tab) {
   if (!verifyTab(tab)) return
 
-  const target = {
-    tabId,
-  }
+  const target = { tabId: tab.id }
+  const insertCSS = (options) =>
+    chrome.scripting.insertCSS({ ...options, target })
+  const removeCSS = (options) =>
+    chrome.scripting.removeCSS({ ...options, target })
 
-  await chrome.scripting.insertCSS({
-    files: ["index.css"],
-    target,
-  })
+  await insertCSS({ files: SHARED_TWEAKS })
 
-  if (tab.url === "https://www.youtube.com/") {
-    await chrome.scripting.insertCSS({
-      files: ["home-page.css"],
-      target,
-    })
+  if (isHomePageURL(tab.url)) {
+    await insertCSS({ files: HOME_PAGE_TWEAKS })
   } else {
-    await chrome.scripting.removeCSS({
-      files: ["home-page.css"],
-      target,
-    })
+    await removeCSS({ files: HOME_PAGE_TWEAKS })
   }
-})
+}
+
+chrome.tabs.onUpdated.addListener(
+  (_0, _1, tab) => handleTabUpdate(tab),
+)
